368756041f5075b501d0f131750d68e479bd7fb6
gomedd99 <gomedd99@aol.com>
2020-07-09 05:03:38 -0500

    3

--- Practica 2/AppGrafo.java
+++ Practica 2/AppGrafo.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class AppGrafo {
     private Grafo G;
     //private int
+    private String Re = " ";
 
 
 
@@ -33,24 +34,59 @@ public class AppGrafo {
         int nodoactual = 0;
         int i = 0;
         Boolean flag ;
-        while (i < nodos.size()) {
-            flag = true;
+        while (i < nodos.size()){
             nodoactual = nodos.get(i);
-            System.out.println(nodos.size());
             for (int j = 1;j <= G.vertices()  ; j++ ) {
+                flag = true;
                 if (G.costo(nodoactual,j) != 0) {
-                    for (int k = 0;k < nodos.size() ; k++ )
-                        if (j == nodos.get(k))
+                    for (int k = 0;k < nodos.size() ; k++ ){
+                        if (j == nodos.get(k)){
                             flag = false;
-                    if (flag)
+                        }
+                    }
+                    if (flag){
                         nodos.add(j);
+                    }
                 }
             }
+            i++;
         }
         Regreso += nodos.toString()+"}";
         return Regreso;
     }
 
+    public String recorridoProfundidad(Grafo G,int u){
+        System.out.println(u);
+        Re += " "+u;
+        System.out.println("this "+Re);
+        int nodoExistente = 0, k = 1;
+        for (int i = 1;i <= G.vertices() ;i++ ) {
+            Boolean flag = true;
+            nodoExistente = 0;
+            k = 1;
+            if (G.costo(u,i) != 0 ) {
+                for (int j = 0;j < Re.length() ;j++ ) {
+                    if(Re.codePointAt(j) != 32){
+                        nodoExistente *= k;
+                        nodoExistente += Character.getNumericValue(Re.charAt(j));
+                        j *=10;
+                    }else {
+                        System.out.println(nodoExistente + " "+i);
+                        if (nodoExistente == i) {
+                            flag = false;
+                            j = Re.length();
+                        }
+                    }
+                }
+                if (flag) {
+                    return recorridoProfundidad(G,i);
+                }
+            }
+
+        }
+        return Re;
+
+    }
 
 
 	public Grafo getG() {
--- Practica 2/main.java
+++ Practica 2/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
         AppGrafo g = new AppGrafo("G1.txt");
-        System.out.println(g.recorridoAmplitud(g.getG(),1));
+        System.out.println(g.recorridoProfundidad(g.getG(),1));
+
     }
 }
