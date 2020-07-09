cb74355713150df69542c4d59290eca3eb38b7c1
gomedd99 <gomedd99@aol.com>
2020-07-09 06:31:08 -0500

    33

--- Practica 2/AppGrafo.java
+++ Practica 2/AppGrafo.java
@@ -9,23 +9,19 @@ public class AppGrafo {
     //private int
     private String Re = " ";
 
-
-
-
-
-	public AppGrafo(String nombre_archivo){
-            try {
-                File fileInput = new File(nombre_archivo);
-                Scanner fileRead = new Scanner(fileInput);
-                G = new Grafo(Integer.valueOf(fileRead.next()));
-                while (fileRead.hasNextDouble()) {
-                    G.agregarArco(Integer.valueOf(fileRead.next()),Integer.valueOf(fileRead.next()),Double.valueOf(fileRead.next()));
-                }
-                fileRead.close();
+    public AppGrafo(String nombre_archivo){
+        try {
+            File fileInput = new File(nombre_archivo);
+            Scanner fileRead = new Scanner(fileInput);
+            G = new Grafo(Integer.valueOf(fileRead.next()));
+            while (fileRead.hasNextDouble()) {
+                G.agregarArco(Integer.valueOf(fileRead.next()),Integer.valueOf(fileRead.next()),Double.valueOf(fileRead.next()));
             }
-            catch (IOException e) {
-            }
-	}
+            fileRead.close();
+        }
+        catch (IOException e) {
+        }
+    }
 
     public String recorridoAmplitud(Grafo G,int u){
         String Regreso = "{";
@@ -64,32 +60,39 @@ public class AppGrafo {
             nodoactual = nodos.get(i);
             for (int j = 1;j <= G.vertices() ; j++ ) {
                 flag = true;
-                 if (G.costo(nodoactual,j) != 0) {
-                     for (int k = 0;k < nodos.size() ; k++ ){
-                         if (j == nodos.get(k)){
-                             flag = false;
-                         }
-                     }
-                     if (flag){
-                         nodos.add(j);
-                         nodoactual = j;
-                         j = 0;
-                     }
-                 }
+                if (G.costo(nodoactual,j) != 0) {
+                    for (int k = 0;k < nodos.size() ; k++ ){
+                        if (j == nodos.get(k)){
+                            flag = false;
+                        }
+                    }
+                    if (flag){
+                        nodos.add(j);
+                        nodoactual = j;
+                        j = 0;
+                    }
+                }
             }
             i++;
         }
         return nodos.toString();
     }
 
+    public String dijkstra(Grafo G ,int u,int v){
+         
+    }
 
-	public Grafo getG() {
-		return G;
-	}
+    public Grafo getG() {
+        return G;
+    }
 
-	public void setG(Grafo G) {
-		this.G = G;
-	}
+    public void setG(Grafo G) {
+        this.G = G;
+    }
+
+
+
+
 
 
 }
--- Practica 2/main.java
+++ Practica 2/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        AppGrafo g = new AppGrafo("G1.txt");
+        AppGrafo g = new AppGrafo("g_highschool.txt");
+        System.out.println(g.getG().aCadena());
         System.out.println(g.recorridoProfundidad(g.getG(),1));
 
     }
